800f71bcc2321748edf0e5d53e8b1e6a2eaa5eb8
Dan Mulvihill <dmulvihill3@gmail.com>
2018-11-06 13:09:34 -0500

    Updates

---
+++ precache-manifest.de507592bdf4d9d9b2fe5943b990b1f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8abffff897efa4abfad2",
+    "url": "/shopping-app/static/js/runtime~main.8abffff8.js"
+  },
+  {
+    "revision": "18c8235372a69e6c97cd",
+    "url": "/shopping-app/static/js/main.18c82353.chunk.js"
+  },
+  {
+    "revision": "a05bd0dad5cd3cdfbb87",
+    "url": "/shopping-app/static/js/1.a05bd0da.chunk.js"
+  },
+  {
+    "revision": "18c8235372a69e6c97cd",
+    "url": "/shopping-app/static/css/main.0b47c334.chunk.css"
+  },
+  {
+    "revision": "a05bd0dad5cd3cdfbb87",
+    "url": "/shopping-app/static/css/1.86531995.chunk.css"
+  },
+  {
+    "revision": "62f68180cc16926fd46109ff66e1c101",
+    "url": "/shopping-app/index.html"
+  }
+];
